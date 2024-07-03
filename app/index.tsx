004d104093afeb8a213de335bfb1df3279bf167b
ADEAGBO MAXWELL <m.adeagbo@sudalys.fr>
2024-07-03 14:31:20 +0200

    stable 0

---
+++ app/index.tsx
@@ -0,0 +1,66 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const Index = () => {
+  const router = useRouter();
+  return (
+    <View className="flex-1 flex justify-end">
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/welcome.png")}
+      />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="flex items-center"
+        >
+          <Text
+            style={{ fontSize: hp(5.5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Les meilleurs<Text className="text-rose-500"> Entraînements</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5.5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Pour vous
+          </Text>
+        </Animated.View>
+        <Animated.View entering={FadeInDown.delay(200).springify()}>
+          <GestureHandlerRootView
+            onPress={() => router.push("/home")}
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[1px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Démarrer !
+            </Text>
+          </GestureHandlerRootView>
+        </Animated.View>
+      </LinearGradient>
+      <StatusBar style="light" />
+    </View>
+  );
+};
+
+export default Index;
